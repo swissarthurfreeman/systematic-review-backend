ba7c79f4c01b5cdb6a8adeff973af2bdb446a8f2
Hugo Haldi <hugo.haldi@etu.unige.ch>
2022-03-18 00:53:43 +0100

    Trying to build with dind

---
+++ src/main/java/ch/unige/pinfo3/Legume.java
@@ -0,0 +1,17 @@
+package ch.unige.pinfo3;
+
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
+public class Legume {
+    public String name;
+    public String description;
+
+    public Legume() {
+    }
+
+    public Legume(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
