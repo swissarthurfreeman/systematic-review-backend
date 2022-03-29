25ceb6433b545186cae4093be983768d1f0a7188
BartaLazar <79648914+BartaLazar@users.noreply.github.com>
2022-03-29 16:30:07 +0200

    add getters and setters to User and transform jobs to dictionnary

--- src/main/java/ch/unige/pinfo3/User.java
+++ src/main/java/ch/unige/pinfo3/User.java
@@ -1,24 +1,33 @@
 package ch.unige.pinfo3;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class User {
     private UUID uuid;
     private String username;
     private String email;
-    private ArrayList<Job> jobs;
+    private Map<UUID, Job> jobs;
 
     public User(String username, String email){
-         uuid = UUID.randomUUID();
-         this.username = username;
-         this.email = email;
-         this.jobs = new ArrayList<Job>();
+        uuid = UUID.randomUUID();
+        this.username = username;
+        this.email = email;
+        this.jobs = new HashMap<UUID, Job>();
     }
 
     public void addJob(Job job){
-        jobs.add(job);
+        jobs.put(job.getId(), job);
     }
 
+    public Job getJob(UUID id){
+        return jobs.get(id);
+    }
+
+    public Map<UUID, Job> getJobs(){
+        return jobs;
+    }
 
 }
