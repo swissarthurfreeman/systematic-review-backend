32c645fde24c612a2d7fd0ecdc10710e1a715a0b
BartaLazar <79648914+BartaLazar@users.noreply.github.com>
2022-03-29 16:07:38 +0200

    add attributes and constructor to User class

--- src/main/java/ch/unige/pinfo3/User.java
+++ src/main/java/ch/unige/pinfo3/User.java
@@ -1,7 +1,20 @@
 package ch.unige.pinfo3;
 
-public class User {
-    public User(String nom){
+import java.util.ArrayList;
+import java.util.UUID;
 
+public class User {
+    private UUID uuid;
+    private String username;
+    private String email;
+    private ArrayList<String> quesries;
+
+    public User(String username, String email){
+         uuid = UUID.randomUUID();
+         this.username = username;
+         this.email = email;
+         this.quesries = new ArrayList<>();
     }
+
+
 }
