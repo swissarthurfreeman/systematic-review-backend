075fc0d0acfe6ea70f606d833b71516544ebfe74
gordon <arthur.freemna@etu.unige.ch>
2022-03-29 16:11:47 +0200

    Added Basic Job class and JobResource Controller Class

---
+++ src/main/java/ch/unige/pinfo3/Job.java
@@ -0,0 +1,18 @@
+package ch.unige.pinfo3;
+
+import java.util.UUID;
+
+public class Job {
+
+    final public String query;
+    final private User user;
+
+    final public UUID id = UUID.randomUUID();
+
+    Job(String query, User user) {
+        this.query = query;
+        this.user = user;
+    }
+
+
+}
