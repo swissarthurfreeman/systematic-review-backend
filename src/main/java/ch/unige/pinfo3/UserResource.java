c1fe786343b64f008cba43a388e32e61b88b0a0c
BartaLazar <79648914+BartaLazar@users.noreply.github.com>
2022-03-29 16:52:13 +0200

    creation and start developpement of UserResource class

---
+++ src/main/java/ch/unige/pinfo3/UserResource.java
@@ -0,0 +1,37 @@
+package ch.unige.pinfo3;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Path("/users")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class UserResource {
+    private Set<User> users = Collections.synchronizedSet(new LinkedHashSet<>());
+
+    public UserResource() {
+        users.add(new User("user1", "user1@unige.ch"));
+        users.add(new User("user2", "user2@etu.unige.ch"));
+    }
+
+    @GET
+    public Response list() {
+        return Response.ok(users).build();
+    }
+
+    @POST
+    public Set<User> add(User user) {
+        users.add(user);
+        return users;
+    }
+
+    @DELETE
+    public Set<User> delete(User user) {
+        users.removeIf(i -> i.getId().equals(user.getId()));
+        return users;
+    }
+}
