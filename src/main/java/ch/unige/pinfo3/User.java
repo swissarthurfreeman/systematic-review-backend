9af26ada09b61c7490ab02f95449f5ee3c6c53d1
BartaLazar <79648914+BartaLazar@users.noreply.github.com>
2022-04-01 16:02:01 +0200

    adding getters in order to make Quarkus return the values

--- src/main/java/ch/unige/pinfo3/User.java
+++ src/main/java/ch/unige/pinfo3/User.java
@@ -1,10 +1,13 @@
 package ch.unige.pinfo3;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+@RegisterForReflection
 public class User {
     private UUID uuid;
     private String username;
@@ -20,6 +23,14 @@ public class User {
 
     public UUID getId(){
         return uuid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void addJob(Job job){
