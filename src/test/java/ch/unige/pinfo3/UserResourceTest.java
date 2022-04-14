71e2ed8558886fc258f2fb126482c0f122d8c7c7
Hugo Haldi <hugo.haldi@etu.unige.ch>
2022-04-14 02:30:29 +0200

    Add H2 for tests

--- src/test/java/ch/unige/pinfo3/UserResourceTest.java
+++ src/test/java/ch/unige/pinfo3/UserResourceTest.java
@@ -1,5 +1,7 @@
 package ch.unige.pinfo3;
 
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.h2.H2DatabaseTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +9,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 
+@QuarkusTestResource(H2DatabaseTestResource.class)
 @QuarkusTest
 public class UserResourceTest {
     @Test
