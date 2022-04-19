f32b76b8d35740c2791d0afddc2dbf423bd66399
BartaLazar <79648914+BartaLazar@users.noreply.github.com>
2022-04-19 13:40:45 +0200

    Tests for users DB

--- src/test/java/ch/unige/pinfo3/UserResourceTest.java
+++ src/test/java/ch/unige/pinfo3/UserResourceTest.java
@@ -21,6 +21,25 @@ public class UserResourceTest {
             .assertThat()
             .statusCode(is(200))
             .and()
-            .body("size()", equalTo(1)); //
+            .body("size()", equalTo(1)); // 1 user in the DB on startup
     }
+
+    @Test
+    //verifie le nb d'attributs pour un utilisateur, et les attributs pour un utilisateur test
+    public void shouldGetUserById(){
+        given()
+                .when()
+                .get("/users/c044a099-e489-43f8-9499-c04a371dbb62")
+                .then()
+                .assertThat()
+                .statusCode(is(200))
+                .and()
+                .body("size()", equalTo(3)) // il y a 3 attributs pour un utilisateur
+                .and()
+                .body("username", equalTo("LazarTest"))
+                .and()
+                .body("email", equalTo("test@lazar.com"));
+    }
+
+
 }
