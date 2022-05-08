89fd841c77810cfabcfc4c852ab4092d6013fc71
BartaLazar <79648914+BartaLazar@users.noreply.github.com>
2022-05-08 18:44:18 +0200

    testing invalid inputs in UserResourceTest.java

--- src/test/java/ch/unige/pinfo3/UserResourceTest.java
+++ src/test/java/ch/unige/pinfo3/UserResourceTest.java
@@ -1,6 +1,7 @@
 package ch.unige.pinfo3;
 
 import ch.unige.pinfo3.domain.model.User;
+import com.github.javafaker.Faker;
 import io.quarkus.logging.Log;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.h2.H2DatabaseTestResource;
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Assertions;
 
 import java.io.InputStream;
+import java.util.UUID;
 
 import static ch.unige.pinfo3.domain.service.UserService.getRandomUser;
 import static io.restassured.RestAssured.get;
@@ -20,7 +22,7 @@ import static org.hamcrest.CoreMatchers.*;
 @QuarkusTestResource(H2DatabaseTestResource.class)
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-public class UserResourceTest{
+class UserResourceTest{
 
     InputStream testUser = getClass().getClassLoader().getResourceAsStream("testUser.json");
 
@@ -28,7 +30,14 @@ public class UserResourceTest{
     // verifier ce qui se pase si je donne, si je donne pas, sie je donne la merde, etc...
     // verifier si je donne un UUID existant
 
+    /// TODO une fois tests passent, faire des tests qui ne passent pas pour voir les codes d'erreur
+
     static User[] testUsers = new User[10];
+
+    @BeforeAll
+    static void logStatus(){
+        Log.info("Testing /user endpoint");
+    }
 
     @BeforeAll
     static void createTestUsers(){
@@ -37,10 +46,11 @@ public class UserResourceTest{
         }
     }
 
+    // Test endpoint POST /users
     @Test
     @Order(1)
-    public void postUsers(){
-        Log.info("Post verification");
+    void postUsers(){
+        Log.info("Test endpoint POST /users");
         Log.info("Populating DB with users");
         for(int i = 0; i < testUsers.length-1; i++){
             User user = testUsers[i];
@@ -67,10 +77,11 @@ public class UserResourceTest{
                 .statusCode(is(200)); // verifie si le post est fait avec succes
     }
 
+    // Test endpoint GET /users
     @Test
     @Order(2)
-    public void getAllUsers() {
-        Log.info("User persistence in db verification");
+    void getAllUsers() {
+        Log.info("Test endpoint GET /users");
         given()
             .when()
             .get("/users")
@@ -82,10 +93,11 @@ public class UserResourceTest{
         Log.info((get("/users").body()).toString());
     }
 
+    // Test endpoint GET /user/id with existing user
     @Test
     @Order(3)
-    public void verifyUser(){
-        Log.info("User data verification");
+    void verifyUser(){
+        Log.info("Test endpoint GET /user/id with existing user");
         given()
                 .when()
                 .get("/users/"+testUsers[testUsers.length-1].uuid)
@@ -99,10 +111,132 @@ public class UserResourceTest{
                 .body("email", equalTo(testUsers[testUsers.length-1].email));
     }
 
+    //Test endpoint GET /user/id with not existing user
+    @Test
+    @Order(4)
+    void getNotExistingUser(){
+        Log.info("Test endpoint GET /user/id with not existing user");
+        given()
+                .when()
+                .get("/users/0")
+                .then()
+                .assertThat()
+                .statusCode(is(400));
+    }
+
+    //Test endpoint POST /user with already existing username
+    @Test
+    @Order(5)
+    void postExistingUserName(){
+        Faker fk = new Faker();
+        Log.info("Test endpoint POST /user with already existing username");
+        String userJson = String.format("{\"username\": \"%s\", \"email\": \"%s\"}",testUsers[1].username, fk.internet().emailAddress());
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(userJson)
+                .when()
+                .post("/users")
+                .then()
+                .assertThat()
+                .statusCode(is(409));
+    }
+
+    //Test endpoint POST /user with already existing email
+    @Test
+    @Order(6)
+    void postExistingUserEmail(){
+        Faker fk = new Faker();
+        Log.info("Test endpoint POST /user with already existing email");
+        String userJson = String.format("{\"username\": \"%s\", \"email\": \"%s\"}",fk.name().username(), testUsers[1].email);
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(userJson)
+                .when()
+                .post("/users")
+                .then()
+                .assertThat()
+                .statusCode(is(409));
+    }
+
+    //Test endpoint POST /user with already existing uuid
+    @Test
+    @Order(7)
+    void postExistingUserUUID(){
+        Faker fk = new Faker();
+        Log.info("Test endpoint POST /user with already existing uuid");
+        String userJson = String.format("{\"uuid\": \"%s\", \"username\": \"%s\", \"email\": \"%s\"}", testUsers[testUsers.length-1].uuid ,fk.name().username(), fk.internet().emailAddress());
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(userJson)
+                .when()
+                .post("/users")
+                .then()
+                .assertThat()
+                .statusCode(is(409));
+    }
+
+    // Test endpoint POST /user with invalid username
+    @Test
+    @Order(8)
+    void postInvalidUserName(){
+        Faker fk = new Faker();
+        Log.info("Test endpoint POST /user with invalid username");
+        String userJson = String.format("{\"username\": \"%s\", \"email\": \"%s\"}","feédàep?fe!|wckowcm", fk.internet().emailAddress());
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(userJson)
+                .when()
+                .post("/users")
+                .then()
+                .assertThat()
+                .statusCode(is(400));
+    }
+
+    // Test endpoint POST /user with invalid email
+    @Test
+    @Order(9)
+    void postInvalidUserEmail(){
+        Faker fk = new Faker();
+        Log.info("Test endpoint POST /user with invalid email");
+        String userJson = String.format("{\"username\": \"%s\", \"email\": \"%s\"}",fk.name().username(), "test[at]test.com");
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(userJson)
+                .when()
+                .post("/users")
+                .then()
+                .assertThat()
+                .statusCode(is(400));
+    }
+
+    // Test endpoint POST /user with invalid uuid
+    @Test
+    @Order(10)
+    void postInvalidUserUUID(){
+        Faker fk = new Faker();
+        Log.info("Test endpoint POST /user with invalid uuid");
+        String userJson = String.format("{\"uuid\": \"%s\", \"username\": \"%s\", \"email\": \"%s\"}", "0123" ,fk.name().username(), fk.internet().emailAddress());
+        given()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(userJson)
+                .when()
+                .post("/users")
+                .then()
+                .assertThat()
+                .statusCode(is(400));
+    }
+
+
     @Test
     @Order(4)
     // test si tous les emails sont valides
-    public void testEmails(){
+    void testEmails(){
         Log.info("User email verification");
         String[] emails = new String[]{};
         emails = get("/users").body().jsonPath().getObject("email",String[].class );
