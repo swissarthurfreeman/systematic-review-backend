604a1356b6bee5a7bc6a3d0ca70ce279a45eb5c1
BartaLazar <79648914+BartaLazar@users.noreply.github.com>
2022-05-26 14:07:35 +0200

    adding elemnts and methods to ResourceTestParent.java, that are usead in all tests

--- src/test/java/ch/unige/pinfo3/ResourceTestParent.java
+++ src/test/java/ch/unige/pinfo3/ResourceTestParent.java
@@ -2,9 +2,11 @@ package ch.unige.pinfo3;
 
 import ch.unige.pinfo3.domain.service.JobService;
 import io.quarkus.test.junit.mockito.InjectMock;
+import io.smallrye.jwt.build.Jwt;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import java.util.UUID;
 
 
 public class ResourceTestParent {
@@ -14,4 +16,11 @@ public class ResourceTestParent {
 
     @InjectMock
     JobService js;
+
+    protected String userUUID = UUID.randomUUID().toString();
+
+    protected String getAccessToken(String userName) {
+        return Jwt.preferredUserName(userName).claim("sub", userUUID).issuer("https://server.example.com")
+                .audience("https://service.example.com").sign();
+    }
 }
