5bcd02753b351f48b23ef3b489be51a5ebcdb751
BartaLazar <79648914+BartaLazar@users.noreply.github.com>
2022-06-09 20:49:14 +0200

    testing GetFractionProgress for job

--- src/test/java/ch/unige/pinfo3/ComponentTesting/JobTest.java
+++ src/test/java/ch/unige/pinfo3/ComponentTesting/JobTest.java
@@ -5,6 +5,8 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static ch.unige.pinfo3.utils.RandomProducer.getRandomJob;
+
 @QuarkusTest
 public class JobTest {
 
@@ -13,5 +15,18 @@ public class JobTest {
         Job job = new Job();
 
         Assertions.assertEquals(job.uuid, job.getUUID());
+    }
+
+    @Test
+    void GetProgressFractionTest(){
+        Job job = getRandomJob();
+
+        if(job.totalTrials != 0){
+            Assertions.assertEquals(job.getProgressFraction(), (double) job.progress/ (double)job.totalTrials);
+        }
+        else{
+            Assertions.assertEquals(job.getProgressFraction(), 0);
+        }
+
     }
 }
